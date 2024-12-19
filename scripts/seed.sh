36666b02d8615c7cb99ca8514d35b9714cb04b05
Rick <192323278+UberRick@users.noreply.github.com>
2024-12-19 17:18:35 +0000

    chore: include more examples in seed data

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -8,6 +8,10 @@ echo "deposit,2,2,2.0" >> "$FILE"
 echo "deposit,1,3,2.0" >> "$FILE"
 echo "withdrawal,1,4,1.5" >> "$FILE"
 echo "withdrawal,2,5,3.0" >> "$FILE"
+echo "dispute,2,2," >> "$FILE"
+echo "chargeback,2,2," >> "$FILE"
+echo "dispute,1,1," >> "$FILE"
+echo "resolve,1,1," >> "$FILE"
 
 # Notify the user
 echo "Seed data written to $FILE"
